9f517a8854343a0d013a777a5a0d6ea80b477b86
chuhx <chuhx1024@126.com>
2025-02-18 17:33:20 +0800

    Feat: side menu done

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,57 +1,141 @@
-import { Outlet } from 'react-router-dom'
-import React from 'react'
-import { Layout } from 'antd'
-const { Header, Footer, Sider, Content } = Layout
+import React, { useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+import { Layout, Menu, Button, Dropdown, Space, Avatar, type MenuProps } from 'antd'
+import { MenuFoldOutlined, MenuUnfoldOutlined, DownOutlined, UserOutlined } from '@ant-design/icons'
+import { SubMenuType, MenuItemType } from 'antd/es/menu/interface'
+import { siderbarRoutes, type siderbarRouteConfig } from '@/router/index'
+
+const layoutStyle = {
+    overflow: 'hidden',
+    width: '100%',
+    maxWidth: '100%',
+    height: '100%',
+}
 
 const headerStyle: React.CSSProperties = {
-    textAlign: 'center',
     color: '#fff',
     height: 64,
-    paddingInline: 48,
     lineHeight: '64px',
-    backgroundColor: '#4096ff',
+    padding: '0  20px 0 0',
+    backgroundColor: '#fff',
+    display: 'flex',
+    justifyContent: 'space-between',
 }
 
 const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-    minHeight: 120,
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#0958d9',
+    minHeight: 'calc(100vh-64px)',
+    overflow: 'auto',
+    padding: '10px',
 }
 
 const siderStyle: React.CSSProperties = {
     textAlign: 'center',
     lineHeight: '120px',
     color: '#fff',
-    backgroundColor: '#1677ff',
+    backgroundColor: '#fff',
 }
 
-const footerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#4096ff',
+const logoStyle: React.CSSProperties = {
+    height: '32px',
+    padding: '16px',
+    margin: '16px',
+    backgroundColor: '#1f1f1f',
 }
 
-const layoutStyle = {
-    borderRadius: 8,
-    overflow: 'hidden',
-    width: '100%',
-    maxWidth: '100%',
-    height: '100%',
-}
 const BasicLayout: React.FC = () => {
+    const { Header, Sider, Content } = Layout
+    const [collapsed, setCollapsed] = useState(false)
+    const navigate = useNavigate()
+
+    const items: MenuProps['items'] = [
+        {
+            key: '1',
+            label: <a onClick={(e) => e.preventDefault()}>退出登录</a>,
+        },
+    ]
+    const onClick: MenuProps['onClick'] = ({ key }) => {
+        if (key === '1') {
+            loginOut()
+        }
+    }
+
+    const loginOut = () => {
+        // 退出登录逻辑
+        // 清除登录状态和缓存
+        // 重定向到登录页面
+        navigate('/login')
+    }
+
+    const generateMenuItems = (routes: siderbarRouteConfig[]): (SubMenuType | MenuItemType)[] => {
+        return routes
+            .filter((route) => !route.meta.hidden)
+            .map((route) => {
+                if (route.children && route.children.length > 0) {
+                    // 如果有子路由，则创建 SubMenuType
+                    return {
+                        key: route.meta.key,
+                        icon: route.meta.icon,
+                        label: route.meta.label,
+                        children: generateMenuItems(route.children as siderbarRouteConfig[]),
+                    } as SubMenuType
+                } else {
+                    // 否则创建 MenuItemType
+                    return {
+                        key: route.meta.key,
+                        icon: route.meta.icon,
+                        label: route.meta.label,
+                    } as MenuItemType
+                }
+            })
+    }
+
+    const menuItems = generateMenuItems(siderbarRoutes)
+
     return (
         <Layout style={layoutStyle}>
-            <Sider width="25%" style={siderStyle}>
-                Sider
+            <Sider style={siderStyle} trigger={null} collapsible collapsed={collapsed}>
+                <div style={logoStyle} />
+                <Menu
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={menuItems}
+                    onClick={(e) => {
+                        const menuKey = e.key
+                        if (menuKey) {
+                            navigate(menuKey)
+                        }
+                    }}
+                />
             </Sider>
             <Layout>
-                <Header style={headerStyle}>Header</Header>
+                <Header style={headerStyle}>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                    <Dropdown menu={{ items, onClick }}>
+                        <a onClick={(e) => e.preventDefault()}>
+                            <Space>
+                                <Avatar
+                                    style={{ backgroundColor: '#87d068' }}
+                                    icon={<UserOutlined />}
+                                />
+                                <span>Admin</span>
+                                <DownOutlined />
+                            </Space>
+                        </a>
+                    </Dropdown>
+                </Header>
                 <Content style={contentStyle}>
                     <Outlet /> {/* 子路由的出口 */}
                 </Content>
-                <Footer style={footerStyle}>Footer</Footer>
             </Layout>
         </Layout>
     )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,147 @@
 import BasicLayout from '@/layouts/BasicLayout'
 import LoginLayout from '@/layouts/LoginLayout'
-
-import { createBrowserRouter } from 'react-router-dom'
-
 import Login from '@/views/login'
+import React from 'react'
+import { VideoCameraOutlined, UploadOutlined, UserOutlined } from '@ant-design/icons'
 
-const router = createBrowserRouter([
+import { createBrowserRouter, RouteObject } from 'react-router-dom'
+
+import { Outlet } from 'react-router-dom'
+
+// 定义 meta 字段的类型
+export interface RouteMeta {
+    key: string
+    hidden?: boolean
+    icon: React.ReactNode
+    label: string
+    requiresAuth?: boolean
+    [key: string]: string | number | boolean | object | null | undefined | React.ReactNode //允许其他自定义属性
+}
+
+export type RouteConfig = RouteObject & {
+    meta?: RouteMeta
+    children?: RouteConfig[]
+}
+export type siderbarRouteConfig = RouteObject & {
+    meta: RouteMeta
+    children?: RouteConfig[]
+}
+export const siderbarRoutes: siderbarRouteConfig[] = [
+    {
+        path: '/dashboard',
+        element: <div>Dashboard</div>,
+        meta: {
+            key: '/dashboard',
+            icon: <VideoCameraOutlined />,
+            label: 'Dashboard',
+        },
+    },
+
+    {
+        path: '/user',
+        element: (
+            <div>
+                <Outlet />
+                User
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+                <div style={{ lineHeight: '100px' }}>测试&lt;Content /&gt;组件滚动</div>
+            </div>
+        ),
+        meta: {
+            key: '/user',
+            icon: <UserOutlined />,
+            label: '用户管理',
+        },
+        children: [
+            {
+                path: '/user/list',
+                element: <div>用户列表</div>,
+                meta: {
+                    key: '/user/list',
+                    icon: <UserOutlined />,
+                    label: '用户列表',
+                },
+            },
+            {
+                path: '/user/add',
+                element: <div>创建用户</div>,
+                meta: {
+                    key: '/user/add',
+                    icon: <UserOutlined />,
+                    label: '新建用户',
+                },
+            },
+        ],
+    },
+    {
+        path: '/role',
+        element: (
+            <div>
+                role123
+                <Outlet />
+            </div>
+        ),
+        meta: {
+            key: '/role',
+            icon: <UserOutlined />,
+            label: '角色管理',
+        },
+        children: [
+            {
+                path: '/role/list',
+                element: <div>角色列表</div>,
+                meta: {
+                    key: '/role/list',
+                    icon: <UserOutlined />,
+                    label: '角色列表',
+                },
+            },
+            {
+                path: '/role/add',
+                element: <div>新建角色</div>,
+                meta: {
+                    key: '/role/add',
+                    icon: <UserOutlined />,
+                    label: '新建角色',
+                },
+            },
+        ],
+    },
+    {
+        path: '/about',
+        element: <div>About</div>,
+        meta: {
+            key: '/about',
+            icon: <UploadOutlined />,
+            label: '关于我们',
+        },
+    },
+    {
+        path: '/about',
+        element: <div>About</div>,
+        meta: {
+            key: '/about',
+            hidden: true,
+            icon: <UploadOutlined />,
+            label: '关于我们',
+        },
+    },
+]
+
+export const routes: RouteConfig[] = [
+    {
+        path: '/',
+        element: <BasicLayout />,
+        children: [...siderbarRoutes],
+    },
     {
         path: '/login',
         element: <LoginLayout />,
@@ -16,20 +152,8 @@ const router = createBrowserRouter([
             },
         ],
     },
-    {
-        path: '/',
-        element: <BasicLayout />,
-        children: [
-            {
-                path: '/',
-                element: <div>Home</div>,
-            },
-            {
-                path: '/about',
-                element: <div>About</div>,
-            },
-        ],
-    },
-])
+]
+
+const router = createBrowserRouter(routes)
 
 export default router
