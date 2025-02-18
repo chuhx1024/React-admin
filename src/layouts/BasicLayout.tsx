cd776810cf1439ebe6d495809f643bff24b984ee
chuhx <chuhx1024@126.com>
2025-02-18 16:03:41 +0800

    Feat: 引入 antd UI 库 并处理 React19 的兼容性问题

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import React from 'react'
+
+import { Button } from 'antd'
 const BasicLayout: React.FC = () => {
     return (
         <>
             <div>123</div>
+            <Button type="primary">Button</Button>
             <Outlet /> {/* 子路由的出口 */}
         </>
     )
