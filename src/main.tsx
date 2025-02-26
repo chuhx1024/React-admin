5a7fb3e12aa770fb861a330f1be1480823aeff0e
chuhx <chuhx1024@126.com>
2025-02-27 04:36:37 +0800

    Feat: 使用 redux-persist 实现 状态管理本地持久化

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import App from './App.tsx'
-import store from './stores' // 引入你创建的 store
+
+import { store, persistor } from './stores' // 引入你创建的 store
+import { PersistGate } from 'redux-persist/integration/react'
 
 import '@ant-design/v5-patch-for-react-19'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </StrictMode>,
 )
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+// import localforage from 'localforage' // 使用 IndexedDB 作为 storage 需要安装 yarn add localforage
+import storage from 'redux-persist/lib/storage' // 使用 localStorage 作为 storage
+
 import appReducer from './modules/appSlice/index'
 import userReducer from './modules/userSlice/index'
 
 const store = configureStore({
     reducer: {
-        user: userReducer,
-        app: appReducer,
+        user: persistReducer({ key: 'user', storage }, userReducer),
+        app: persistReducer({ key: 'app', storage }, appReducer),
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST'],
+            },
+        }),
 })
 
-export default store
+// 创建 persistor
+const persistor = persistStore(store)
+
+export { store, persistor }
